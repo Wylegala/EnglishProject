5fc8fe6678e8f672d77131728bf6cc5db4d0be66
Michal Wylegala <wylegala.michall@gmail.com>
2019-04-09 22:04:57 +0200

    Added DB connection mechanism

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+	//Database login data
+
+	$host = "localhost";
+	$db_user = "root";
+	$db_password = "";
+	$db_name = "it-lexicon";
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+
+  session_start();
+
+  require_once "connect.php";
+
+  $connection = @new mysqli($host, $db_user, $db_password, $db_name);
+  if ($connection->connect_errno!=0)
+  {
+    echo "Error: ".$connection->connect_errno;
+  }
+  else
+  {
+    // code...
+  }
+
+  $connection->close();
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
