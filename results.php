9cfbca7a3fdf1a7bd13b30d32d83c6ff560db02a
Michal Wylegala <wylegala.michall@gmail.com>
2019-04-21 23:51:36 +0200

    Styled results page. Fixed footer

--- results.php
+++ results.php
@@ -61,69 +61,68 @@
         </div>
       </div>
       <!--Page body-->
-      <div id="content">
-				<div id="search_results">
-          <div class="container">
-		        <?php
-							//open DB connection
-							$connection = @new mysqli($host, $db_user, $db_password, $db_name);
-						  if ($connection->connect_errno!=0)
-						  {
-								//Error handler in case of unsuccessfull connection
-						    echo "Error: ".$connection->connect_errno;
-						  }
-						  else
-						  {
-								//Defining $search that will be used later on to refine data from DB
-								if(isset($_POST['search']))
+			<div id="results">
+				<div class="container">
+					<?php
+						//open DB connection
+						$connection = @new mysqli($host, $db_user, $db_password, $db_name);
+						if ($connection->connect_errno!=0)
+						{
+							//Error handler in case of unsuccessfull connection
+							echo "Error: ".$connection->connect_errno;
+							exit();
+						}
+						else
+						{
+							//Defining $search that will be used later on to refine data from DB
+							if(isset($_POST['search']))
+							{
+								$search = $_POST['search'];
+							}
+							elseif(isset($_GET['search']))
+							{
+								$search = $_GET['search'];
+							}
+
+							//Sanitization of entered phrase
+							$search = htmlentities($search, ENT_QUOTES, "UTF-8");
+
+							//DB query, search for mentioned term
+							if ($result = @$connection->query(
+							sprintf("SELECT * FROM terms WHERE name='%s'",
+							mysqli_real_escape_string($connection,$search))))
+							{
+								//Count and verify if any results
+								$count_results = $result->num_rows;
+								if($count_results>0)
 								{
-									$search = $_POST['search'];
-								}
-								elseif(isset($_GET['search']))
-								{
-									$search = $_GET['search'];
-								}
+									//Get and display the term
+									$term = $result->fetch_assoc();
 
-								//Sanitization of entered phrase
-								$search = htmlentities($search, ENT_QUOTES, "UTF-8");
+									echo "<div class='result_name'>".$term['name']."</div>";
+									echo "<div class='result_desc'>".$term['description']."</div>";
 
-								//DB query, search for mentioned term
-								if ($result = @$connection->query(
-								sprintf("SELECT * FROM terms WHERE name='%s'",
-								mysqli_real_escape_string($connection,$search))))
-								{
-									//Count and verify if any results
-									$count_results = $result->num_rows;
-									if($count_results>0)
-									{
-										//Get and display the term
-										$term = $result->fetch_assoc();
-
-										echo "<p class='term_name'>".$term['name']."</p>";
-										echo "<p class='term_desc'>".$term['description']."</p>";
-
-										//Increment popularity for given term
-										$id = $term['id_terms'];
-										$connection->query("UPDATE terms SET popularity=popularity+1 WHERE id_terms='$id'");
-									}
-									else
-									{
-										//Error handler - cannot find term in the DB
-										echo "<p class='term_name' style='font-size: 2rem;'>".$search." cannot be found. Please, try another term.</p>";
-									}
+									//Increment popularity for given term
+									$id = $term['id_terms'];
+									$connection->query("UPDATE terms SET popularity=popularity+1 WHERE id_terms='$id'");
 								}
 								else
 								{
-									header('Location: index.php');
-									exit();
+									//Error handler - cannot find term in the DB
+									echo "<p class='term_name' style='font-size: 2rem;'>".$search." cannot be found. Please, try another term.</p>";
 								}
-						  }
-						?>
-					</div>
+							}
+							else
+							{
+								header('Location: index.php');
+								exit();
+							}
+						}
+					?>
 				</div>
-      </div>
+			</div>
       <!--Footer section-->
-      <div id="footer">
+      <div id="footer" class="footer_fixed">
         <div class="container">
           Footer placeholder
         </div>
